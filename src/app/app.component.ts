d75cc47cc56bdf8cbd1d7aad4f6112f7f8fc8bd8
Hasnain <mhasnain1727@gmail.com>
2025-12-10 00:02:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shivangel-website';
+  title = 'Shivangel Ayurveda & Nutraceuticals';
+  isMenuOpen = false;
+  currentYear = new Date().getFullYear();
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  scrollTo(sectionId: string): void {
+    const el = document.getElementById(sectionId);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+    this.isMenuOpen = false;
+  }
+
+  call(number: string): void {
+    window.location.href = `tel:${number}`;
+  }
 }
